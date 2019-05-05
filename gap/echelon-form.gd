219279d56f9e6106d25365ab869921ecc2785afc
Emma Ahrens <emmakatherina@icloud.com>
2019-05-05 23:33:12 +0200

    Add examples for low level functions

--- gap/echelon-form.gd
+++ gap/echelon-form.gd
@@ -8,3 +8,19 @@
 #! height, width and rank over the given ring. One random matrix can be
 #! recreated by using the same random seed.
 DeclareGlobalFunction( "RandomEchelonMat" );
+#! @BeginExampleSession
+#! gap> # We use Mersenne twister as a random seed here.
+#! gap> randomSeed := RandomSource(IsMersenneTwister, 42);;
+#! gap> M := RandomEchelonMat( 10, 10, 5, randomSeed, GF( 5 ) );;
+#! gap> Display( M );
+#! 1 . . . . . 1 4 4 .
+#! . 1 . . . 1 3 2 4 3
+#! . . 1 . . . 3 . 1 .
+#! . . . 1 . 2 2 3 4 2
+#! . . . . 1 2 2 4 1 4
+#! . . . . . . . . . .
+#! . . . . . . . . . .
+#! . . . . . . . . . .
+#! . . . . . . . . . .
+#! . . . . . . . . . .
+#! @EndExampleSession
