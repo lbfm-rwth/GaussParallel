6f8f311d52e2dae92e90166953881d683e7e8ce6
Emma Ahrens <emmakatherina@icloud.com>
2019-05-05 18:33:42 +0200

    Document a few of the low level functions

--- gap/echelon-form.gd
+++ gap/echelon-form.gd
@@ -2,4 +2,9 @@
 #! @Chapter Functions
 #! @Section Low-Level Functions
 #!
+#! @Arguments height, width, rank, randomSeed, ring
+#! @Returns mat
+#! @Description Constructs a random matrix in echelon form of the given
+#! height, width and rank over the given ring. One random matrix can be
+#! recreated by using the same random seed.
 DeclareGlobalFunction( "RandomEchelonMat" );
