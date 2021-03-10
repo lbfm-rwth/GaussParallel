bab5f70cd681ec9a42ac249de19446b83a4799db
jnkbrachter <34373173+jnkbrachter@users.noreply.github.com>
2021-03-10 17:54:54 +0100

    Documentation

--- gap/main.gd
+++ gap/main.gd
@@ -1,91 +1,54 @@
-# Declare main global functions
-
-#! @Chapter Gaussian Elimination
-#! @Section Gaussian Elimination
-#!
-#! @Arguments mat
-#! @Returns a record that contains information of an echelonized version of mat.
-#! @Description This record contains
-#! * vectors:
-#!   the reduced row echelon form of the matrix mat without the zero rows
-#! * heads:
-#!    list that contains at position i, if nonzero, the number of the row for
-#!    that the pivot element is in column i.
-#! * coeffs:
-#!   the transformation matrix needed to obtain the RREF from mat
-#! * relations:
-#!   the kernel of the matrix mat if the underlying ring is a field
-#!
-#! Calculates the parallel Gauss algorithm. This version assumes the underlying
-#! field of the matrix by using DefaultFieldOfMatrix and uses a block size that
-#! has proven to lead to the fastest execution of the algorithm. It returns the
-#! commonly needed informations.
-DeclareGlobalFunction( "EchelonMatTransformationBlockwise" );
-
-#! @Arguments mat
-#! @Returns a record that contains information of an echelonized version of mat.
-#! @Description This record contains
-#! * vectors:
-#!   the reduced row echelon form of the matrix mat without the zero rows
-#! * heads:
-#!    list that contains at position i, if nonzero, the number of the row for
-#!    that the pivot element is in column i.
-#!
-#! Calculates the parallel Gauss algorithm. This version assumes the underlying
-#! field of the matrix by using DefaultFieldOfMatrix and uses a block size that
-#! has proven to lead to the fastest execution of the algorithm. It returns
-#! a minimum of information but has the least execution time.
-DeclareGlobalFunction( "EchelonMatBlockwise" );
-
-#! @Chapter Low-Level Functions
-#! @Section Low-Level Functions
-#!
-#!
 if IsHPCGAP then
      MakeReadOnlyOrImmutableObj := MakeReadOnlyObj;
 else
      MakeReadOnlyOrImmutableObj := MakeImmutable;
 fi;
 
-#! @Arguments mat, options
-#! @Returns a record
-#! @Description This record contains
-#! * vectors:
-#!   the reduced row echelon form of the matrix mat without the zero rows
-#! * pivotrows and pivotcols:
-#!   a list where the rows resp. columns with pivot elements are marked with 1
-#!   whereas the other entries are 0
-#! * rank:
-#!   the rank of the matrix mat
-#! * heads:
-#!    list that contains at position i, if nonzero, the number of the row for
-#!    that the pivot element is in column i.
-#! and optionally
-#! * coeffs:
-#!   the transformation matrix needed to obtain the RREF from mat
-#! * relations:
-#!   the kernel of the matrix mat if the underlying ring is a field
-#! * transformation:
-#!   a transformation matrix needed to obtain the RREF from mat
-#!   (filled with vectors that contain only zeros)
-#!
-#! Calculates the parallel Gauss algorithm. This is the version with the most
-#! options to specify the functionality. You can specify how the algorithm
-#! works and which return values it computes using the argument options which
-#! is a record that may contain the following fields:
-#! * galoisField:
-#!   the galois field of the matrix, e.g. GF(7)
-#! * numberBlocksHeight and numberBlocksWidth:
+# Declare main global functions
+#! @Chapter Gaussian Elimination
+#! @Section Gaussian Elimination
+
+
+#! @Arguments mat, [options]
+#! @Returns a record that contains information on the echelon form of **mat** and the corresponding transformation matrix.
+#! @Description This is the main function of the GaussPar package. It computes the reduced row echelon form (RREF) of the matrix **mat** and the corresponding transformation matrix. In a pre-processing step, **mat** is split up into smaller block matrices which can be processed in parallel.
+#! TODO Explain how to compoute Trafo as Concat(coeffs,relations);
+#!  
+#!  The output record contains the following items:
+#! * **vectors**:
+#!   a matrix that forms the RREF of **mat** without zero rows
+#! * **heads**:
+#!    a list of integers, such that **heads[i]** gives the number of the row for
+#!    which the pivot element is in column i. If no such row exists, **heads[i]** is **0**.
+#! * **coeffs**:
+#!   the corresponding transformation matrix. It holds **coeffs** * **mat** = **vectors**.
+#! * **relations**: the kernel of the matrix **mat**. If **relations** is not the  empty list, it holds **relations** * **mat** = **0**. Otherwise **mat** has full row rank.
+#! 
+#!  The input parameters have the following meaning:
+#! * **mat** is a matrix defined over a finite field
+#! * **options** is a record that can be used to provide some additional parameters. The following   are currently supported:
+#!   * **numberBlocksHeight** and **numberBlocksWidth**:
 #!   The number of vertical and horizontal blocks in which to divide
-#!   the matrix during the algorithm, note that you need to specify either none
+#!   the matrix during the algorithm. Note that you need to specify either none
 #!   or both of those variables to make it work.
-#! * numberBlocks:
-#!   Use this argument if you want the same number of vertical and horizontal chops.
-#! * withTrafo:
-#!   A boolean specifying whether or not the transformation matrix is
-#!   calculated, the default value is true.
-#! * isChopped: I don't know what this does!!! TODO
-#! * verify:
-#!   A boolean specifying whether or not the result values should be directly
-#!   checked, the default value is false.
+#!   * **numberBlocks**:
+#!   Use this argument if you want the same number of vertical and horizontal blocks in the block matrix decomposition of **mat**.
+#!   * **verify**: If set to **true**, the computation is verified at the end. That is, we check wheter **coeffs** * **mat** is in RREF. This option is only available for the function **EchelonMatTransformationBlockwise**.
+#!   * **isChopped**: It is possible to input **mat** directly as a matrix of block matrices. In this case the parameter **isChopped** must be set to **true** and the splitting step is skipped. Note that a specification of **numberBlocks** is mandatory if **isChopped** is set to **true**. 
+DeclareGlobalFunction( "EchelonMatTransformationBlockwise" );
+
+#! @Arguments mat, [options]
+#! @Returns a record that contains information on the echelon form of **mat**.
+#! @Description This is a version of the main function that computes the reduced row echelon form (RREF) of the matrix **mat** but doesn't compute the corresponding transformation matrix. In a pre-processing step, **mat** is split up into smaller block matrices which can be processed in parallel.
+#!  
+#!  The output record contains the following items:
+#! * **vectors**
+#! * **heads**
+#! For their meaning, see **EchelonMatTransformationBlockwise**.
+#! 
+#!  The input parameters have the following meaning:
+#! * **mat** is a matrix defined over a finite field
+#! * **options** is a record that can be used to provide some additional parameters. For more information see **EchelonMatTransformationBlockwise** (TODO:Use cross-ref here).
+DeclareGlobalFunction( "EchelonMatBlockwise" );
+
 DeclareGlobalFunction( "DoEchelonMatTransformationBlockwise" );
--- gap/upstream.gd
+++ gap/upstream.gd
@@ -1,6 +1,2 @@
-# Declare global functions
-#! @Chapter Utility Functions
-#! @Section Utility Functions
-#!
 DeclareGlobalFunction( "GAUSS_EchelonBasisMutableTX" );
 DeclareGlobalFunction( "GAUSS_EchelonBasisMutableT" );
