c06f23ed34e3f7df2897ad3d1431f3cc04e54484
Jendrik Brachter <brachter@rhrk.uni-kl.de>
2021-03-10 17:49:09 +0100

    Some fixes

--- gap/main.gd
+++ gap/main.gd
@@ -1,5 +1,10 @@
-# Declare main global functions
+if IsHPCGAP then
+     MakeReadOnlyOrImmutableObj := MakeReadOnlyObj;
+else
+     MakeReadOnlyOrImmutableObj := MakeImmutable;
+fi;
 
+# Declare main global functions
 #! @Chapter Gaussian Elimination
 #! @Section Gaussian Elimination
 
@@ -7,6 +12,7 @@
 #! @Arguments mat, [options]
 #! @Returns a record that contains information on the echelon form of **mat** and the corresponding transformation matrix.
 #! @Description This is the main function of the GaussPar package. It computes the reduced row echelon form (RREF) of the matrix **mat** and the corresponding transformation matrix. In a pre-processing step, **mat** is split up into smaller block matrices which can be processed in parallel.
+#! TODO Explain how to compoute Trafo as Concat(coeffs,relations);
 #!  
 #!  The output record contains the following items:
 #! * **vectors**:
@@ -21,13 +27,13 @@
 #!  The input parameters have the following meaning:
 #! * **mat** is a matrix defined over a finite field
 #! * **options** is a record that can be used to provide some additional parameters. The following   are currently supported:
-#! * **numberBlocksHeight** and **numberBlocksWidth**:
+#!   * **numberBlocksHeight** and **numberBlocksWidth**:
 #!   The number of vertical and horizontal blocks in which to divide
-#!   the matrix during the algorithm, note that you need to specify either none
+#!   the matrix during the algorithm. Note that you need to specify either none
 #!   or both of those variables to make it work.
-#! * **numberBlocks**:
-#!   Use this argument if you want the same number of vertical and horizontal chops.
-#!   * **verify**: If set to **true**, the computation will be verified at the end.
+#!   * **numberBlocks**:
+#!   Use this argument if you want the same number of vertical and horizontal blocks in the block matrix decomposition of **mat**.
+#!   * **verify**: If set to **true**, the computation is verified at the end. That is, we check wheter **coeffs** * **mat** is in RREF. This option is only available for the function **EchelonMatTransformationBlockwise**.
 #!   * **isChopped**: It is possible to input **mat** directly as a matrix of block matrices. In this case the parameter **isChopped** must be set to **true** and the splitting step is skipped. Note that a specification of **numberBlocks** is mandatory if **isChopped** is set to **true**. 
 DeclareGlobalFunction( "EchelonMatTransformationBlockwise" );
 
@@ -36,67 +42,13 @@ DeclareGlobalFunction( "EchelonMatTransformationBlockwise" );
 #! @Description This is a version of the main function that computes the reduced row echelon form (RREF) of the matrix **mat** but doesn't compute the corresponding transformation matrix. In a pre-processing step, **mat** is split up into smaller block matrices which can be processed in parallel.
 #!  
 #!  The output record contains the following items:
-#! * **vectors**:
-#!   a matrix that forms the RREF of **mat** without zero rows
-#! * **heads**:
-#!    a list of integers, such that **heads[i]** gives the number of the row for
-#!    which the pivot element is in column i. If no such row exists, **heads[i]** is **0**.
+#! * **vectors**
+#! * **heads**
+#! For their meaning, see **EchelonMatTransformationBlockwise**.
 #! 
 #!  The input parameters have the following meaning:
 #! * **mat** is a matrix defined over a finite field
 #! * **options** is a record that can be used to provide some additional parameters. For more information see **EchelonMatTransformationBlockwise** (TODO:Use cross-ref here).
 DeclareGlobalFunction( "EchelonMatBlockwise" );
 
-#! @Chapter Low-Level Functions
-#! @Section Low-Level Functions
-#!
-#!
-#! @Arguments mat, options
-#! @Returns a record that contains information on the echelon form of **mat**.
-#! @Description This record contains
-#! * **vectors**:
-#!   the reduced row echelon form of the matrix **mat** without the zero rows
-#! * **pivotrows** and **pivotcols**:
-#!   a list where the rows resp. columns with pivot elements are marked with 1
-#!   whereas the other entries are 0
-#! * **rank**:
-#!   the rank of the matrix **mat**
-#! * **heads**:
-#!    list that contains at position i, if nonzero, the number of the row for
-#!    that the pivot element is in column i.
-#! and optionally
-#! * **coeffs**:
-#!   the transformation matrix needed to obtain the RREF from mat
-#! * **relations**:
-#!   the kernel of the matrix mat if the underlying ring is a field
-#! * **transformation**:
-#!   a transformation matrix needed to obtain the RREF from mat
-#!   (filled with vectors that contain only zeros)
-#!
-#! This function executes the parallel Gaussian algorithm. Both **EchelonMatTransformationBlockwise** and **EchelonMatBlockwise** are wrapper functions of it.
-#! This is the version with the most
-#! options to specify functionality. You can specify how the algorithm
-#! works and which return values it computes using the argument options which
-#! is a record that may contain the following items:
-#! * **galoisField**:
-#!   the galois field of the matrix, e.g. GF(7)
-#! * **numberBlocksHeight** and **numberBlocksWidth**:
-#!   The number of vertical and horizontal blocks in which to divide
-#!   the matrix during the algorithm, note that you need to specify either none
-#!   or both of those variables to make it work.
-#! * **numberBlocks**:
-#!   Use this argument if you want the same number of vertical and horizontal chops.
-#! * **withTrafo**:
-#!   A boolean specifying whether or not the transformation matrix is
-#!   computed, the default value is true.
-#!   * **isChopped**: It is possible to input **mat** directly as a matrix of block matrices. In this case the parameter **isChopped** must be set to **true** and the splitting step is skipped. Note that a specification of **numberBlocks** is mandatory if **isChopped** is set to **true**.
-#! * **verify**:
-#!   A boolean specifying whether or not the result values should be directly
-#!   checked, the default value is false.
 DeclareGlobalFunction( "DoEchelonMatTransformationBlockwise" );
-
-if IsHPCGAP then
-     MakeReadOnlyOrImmutableObj := MakeReadOnlyObj;
-else
-     MakeReadOnlyOrImmutableObj := MakeImmutable;
-fi;
