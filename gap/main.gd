f6031737bdac0da00b86af1a89782268cda20dcf
Jendrik Brachter <brachter@rhrk.uni-kl.de>
2021-04-16 15:06:56 +0200

    change order of inputs and outputs in doc descrition of main functions

--- gap/main.gd
+++ gap/main.gd
@@ -13,21 +13,6 @@ fi;
 #! @Returns a record that contains information on the echelon form of **mat** and the corresponding transformation matrix.
 #! @Description This is the main function of the GaussPar package. It computes the reduced row echelon form (RREF) of the matrix **mat** and the corresponding transformation matrix. In a pre-processing step, **mat** is split up into a block matrix whose blocks can be processed in parallel.
 #!
-#!  The output record contains the following items:
-#! * **vectors**:
-#!   a matrix that forms the RREF of **mat** without zero rows
-#! * **heads**:
-#!    a list of integers, such that **heads[i]** gives the number of the row for
-#!    which the pivot element is in column i. If no such row exists, **heads[i]** is **0**.
-#! * **coeffs**:
-#!   the corresponding transformation matrix. It holds **coeffs** * **mat** = **vectors**.
-#! * **relations**: a matrix whose rows form a basis for the row null space of **mat**. If **relations** is not the  empty list, it holds that **relations** * **mat** = **0**. Otherwise **mat** has full row rank.
-#!  
-#!  The transformation matrix can be easily obtained from the output record as follows:
-#! @BeginExampleSession
-#! gap> Concatenation(result.coeffs, result.relations);
-#! @EndExampleSession
-#!
 #!  The input parameters have the following meaning:
 #! * **mat** is a matrix defined over a finite field
 #! * **options** is a record that can be used to provide some additional parameters.  Note that a specification of either **numberBlocks** or both of
@@ -39,6 +24,18 @@ fi;
 #!   Use this argument if you want the same number of vertical and horizontal blocks in the block matrix decomposition of **mat**.
 #!   * **verify**: If set to **true**, the computation is verified at the end. That is, we check wheter **coeffs** * **mat** is in RREF. This option is only available for the function **EchelonMatTransformationBlockwise**.
 #!   * **isChopped**: It is possible to input **mat** directly as a matrix of block matrices. In this case the parameter **isChopped** must be set to **true** and the splitting step is skipped.
+#!
+#!  The output record contains the following items:
+#! * **vectors**:
+#!   a matrix that forms the RREF of **mat** without zero rows
+#! * **heads**:
+#!    a list of integers, such that **heads[i]** gives the number of the row for
+#!    which the pivot element is in column i. If no such row exists, **heads[i]** is **0**.
+#! * **coeffs**:
+#!   the corresponding transformation matrix. It holds **coeffs** * **mat** = **vectors**.
+#! * **relations**: a matrix whose rows form a basis for the row null space of **mat**. If **relations** is not the  empty list, it holds that **relations** * **mat** = **0**. Otherwise **mat** has full row rank.
+#!  
+#!
 #! @BeginExampleSession
 #! gap> A := RandomMat(8, 5, GF(5)) * RandomMat(5, 8, GF(5));;
 #! gap> Display(A);
@@ -59,6 +56,9 @@ fi;
 #! . . . . 1 2 2 2
 #! gap> res.coeffs * A=res.vectors;
 #! true
+#! @EndExampleSession
+#!  The transformation matrix can be easily obtained from the output record as follows:
+#! @BeginExampleSession
 #! gap> trafo := Concatenation(res.coeffs, res.relations);;
 #! gap> Display(trafo * A);
 #! 1 . . . . . . 3
@@ -89,12 +89,12 @@ DeclareGlobalFunction( "EchelonMatTransformationBlockwise" );
 #! @Arguments mat, [options]
 #! @Returns a record that contains information on the echelon form of **mat**.
 #! @Description This is a version of the main function that computes the reduced row echelon form (RREF) of the matrix **mat** but doesn't compute the corresponding transformation matrix. In a pre-processing step, **mat** is split up into a block matrix whose blocks can be processed in parallel.
-#!  
-#!  The output record contains the items **vectors** and **heads**. For their meaning, see <Ref Func="EchelonMatTransformationBlockwise"/>.
 #! 
 #!  The input parameters have the following meaning:
 #! * **mat** is a matrix defined over a finite field
 #! * **options** is a record that is used to provide additional parameters. For more information see **EchelonMatTransformationBlockwise** (See  <Ref Func="EchelonMatTransformationBlockwise"/>).
+#!  
+#!  The output record contains the items **vectors** and **heads**. For their meaning, see <Ref Func="EchelonMatTransformationBlockwise"/>.
 DeclareGlobalFunction( "EchelonMatBlockwise" );
 
 DeclareGlobalFunction( "DoEchelonMatTransformationBlockwise" );
